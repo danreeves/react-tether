58eedcf4cb28c6b3f5d4204680bcc36f1fd0e9fe
Travis Arnold <ftntravis@gmail.com>
2015-11-08 12:43:48 -0800

    feeling out API

--- example/index.jsx
+++ example/index.jsx
@@ -1,13 +1,108 @@
 import React, { Component, Children, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
-import TetherElement from '../src/react-tether'
+import TetherTarget from '../src/react-tether'
 import './main.scss'
 
+const content = (extraContent) => (
+  <div
+    style={{
+      padding: 12,
+      background: 'orange'
+    }}
+  >
+    <i>I'm tethered!</i>
+    {
+      extraContent &&
+      <div>Extra Content</div>
+    }
+  </div>
+)
+
 class App extends Component {
+  state = {
+    isOpen: false,
+    toggleHeight: false,
+    position: 'top',
+    align: 'left',
+    toggleContent: false
+  }
+
+  componentDidMount() {
+    // position example in the middle
+    this.refs.example.scrollLeft = 925
+    this.refs.example.scrollTop = 925
+    
+    // allow correct position of drop after first render
+    this.forceUpdate()
+  }
+
   render() {
+    const { isOpen, toggleHeight, position, align, toggleContent } = this.state
+
     return(
-      <div></div>
-    )
+      <div className="app">
+        <button onClick={() => this.setState({isOpen: !isOpen})}>
+          Toggle Drop
+        </button>
+        <select
+          value={position}
+          onChange={
+            e => this.setState({position: e.target.value})
+          }
+        >
+          <option value="top">Top</option>
+          <option value="right">Right</option>
+          <option value="bottom">Bottom</option>
+          <option value="left">Left</option>
+        </select>
+        <select
+          value={align}
+          onChange={
+            e => this.setState({align: e.target.value})
+          }
+        >
+          <option value="left">Left</option>
+          <option value="middle">Middle</option>
+          <option value="right">Right</option>
+          <option value="top">Top</option>
+          <option value="bottom">Bottom</option>
+        </select>
+        <button onClick={() => this.setState({toggleContent: !this.state.toggleContent})}>
+          Toggle Drop Content
+        </button>
+        <button onClick={() => this.setState({toggleHeight: !this.state.toggleHeight})}>
+          {toggleHeight ? <span>Decrease Height</span> : <span>Increase Height</span>}
+        </button>
+
+        <div ref="example" className="drop-example">
+          <div className="drop-scroll-content">
+            <TetherTarget
+              tethered={content(isOpen && toggleContent)}
+              tetherOptions={{
+                attachment: 'top center',
+                constraints: [
+                  {
+                    to: 'scrollParent',
+                    attachment: 'together'
+                  }
+                ]
+              }}
+            >
+              <div
+                ref="target"
+                style={{
+                  width: 200,
+                  height: toggleHeight ? 300 : 200,
+                  padding: 12,
+                  background: '#b4da55',
+                }}
+                onClick={() => this.setState({isOpen: !isOpen})}
+              />
+            </TetherTarget>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Tether.jsx
+++ src/Tether.jsx
@@ -1,9 +1,83 @@
-import React, { Component, Children, PropTypes, cloneElement } from 'react'
+import React, { Component, Children, PropTypes, createElement } from 'react'
 import ReactDOM from 'react-dom'
+import Tether from 'tether'
+
+// TetherElement
+// TetherTarget
+// 
+// <TetherElement
+//   target={this.refs.trigger}
+//   options={{}}
+// >
+//   <div>
+//     I'm a cool box that's tethered to a trigger
+//   </div>
+// <TetherElement>
+
+class TetheredElement {
+  constructor(reactComponent, tetherOptions) {
+    this.reactComponent = reactComponent
+
+    this._node = document.createElement('div')
+    this._node.style.position = 'absolute'
+    
+    document.body.appendChild(this._node)
+
+    this.tether = new Tether({
+      element: this._node,
+      ...tetherOptions
+    })
+
+    this.update()
+  }
+
+  update() {
+    ReactDOM.render(
+      this.reactComponent,
+      this._node,
+      () => this.tether.position()
+    )
+  }
+
+  destroy() {
+    React.unmountComponentAtNode(this._node)
+    this.domNode.parentNode.removeChild(this._node)
+    this.tether.destroy()
+  }
+}
 
 class TetherElement extends Component {
+  static propTypes = {
+    tethered: React.PropTypes.node.isRequired,
+    tetherOptions: React.PropTypes.object.isRequired
+  }
+
+  state = {
+    tooltipVisible: false
+  }
+
+  componentDidMount() {
+    const tetherOptions = {
+      target: ReactDOM.findDOMNode(this),
+      ...this.props.tetherOptions
+    }
+
+    this._tethered = new TetheredElement(
+      this.props.tethered, tetherOptions
+    )
+  }
+
+  componentDidUpdate() {
+    this._tethered.update()
+  }
+
+  componentWillUnmount() {
+    this._tethered.destroy()
+  }
+
   render() {
-    return this.props.children
+    const { tethered, tetherOptions, children, ...props } = this.props
+    return Children.only(children)
   }
 }
 
