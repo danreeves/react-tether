518077c71e20dcb40a93862b8f6a31ebbab1dd4d
Travis Arnold <ftntravis@gmail.com>
2015-11-08 14:11:04 -0800

    clean up example to test it works

--- example/index.jsx
+++ example/index.jsx
@@ -1,29 +1,14 @@
 import React, { Component, Children, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
-import TetherTarget from '../src/react-tether'
+import TetherElement from '../src/react-tether'
 import './main.scss'
-
-const content = (extraContent) => (
-  <div
-    style={{
-      padding: 12,
-      background: 'orange'
-    }}
-  >
-    <i>I'm tethered!</i>
-    {
-      extraContent &&
-      <div>Extra Content</div>
-    }
-  </div>
-)
 
 class App extends Component {
   state = {
-    isOpen: false,
+    isOpen: true,
     toggleHeight: false,
-    position: 'top',
-    align: 'left',
+    vertical: 'top',
+    horizontal: 'left',
     toggleContent: false
   }
 
@@ -37,68 +22,82 @@ class App extends Component {
   }
 
   render() {
-    const { isOpen, toggleHeight, position, align, toggleContent } = this.state
+    const { isOpen, toggleHeight, vertical, horizontal, toggleContent } = this.state
 
     return(
       <div className="app">
-        <button onClick={() => this.setState({isOpen: !isOpen})}>
-          Toggle Drop
-        </button>
-        <select
-          value={position}
-          onChange={
-            e => this.setState({position: e.target.value})
-          }
-        >
-          <option value="top">Top</option>
-          <option value="right">Right</option>
-          <option value="bottom">Bottom</option>
-          <option value="left">Left</option>
-        </select>
-        <select
-          value={align}
-          onChange={
-            e => this.setState({align: e.target.value})
-          }
-        >
-          <option value="left">Left</option>
-          <option value="middle">Middle</option>
-          <option value="right">Right</option>
-          <option value="top">Top</option>
-          <option value="bottom">Bottom</option>
-        </select>
-        <button onClick={() => this.setState({toggleContent: !this.state.toggleContent})}>
-          Toggle Drop Content
-        </button>
-        <button onClick={() => this.setState({toggleHeight: !this.state.toggleHeight})}>
-          {toggleHeight ? <span>Decrease Height</span> : <span>Increase Height</span>}
-        </button>
+        <header className="app__header">
+          <button onClick={() => this.setState({isOpen: !isOpen})}>
+            Toggle Drop
+          </button>
+          <select
+            value={vertical}
+            onChange={
+              e => this.setState({vertical: e.target.value})
+            }
+          >
+            <option value="top">Top</option>
+            <option value="middle">Middle</option>
+            <option value="bottom">Bottom</option>
+          </select>
+          <select
+            value={horizontal}
+            onChange={
+              e => this.setState({horizontal: e.target.value})
+            }
+          >
+            <option value="left">Left</option>
+            <option value="center">Center</option>
+            <option value="right">Right</option>
+          </select>
+          <button onClick={() => this.setState({toggleContent: !this.state.toggleContent})}>
+            Toggle Drop Content
+          </button>
+          <button onClick={() => this.setState({toggleHeight: !this.state.toggleHeight})}>
+            {toggleHeight ? <span>Decrease Height</span> : <span>Increase Height</span>}
+          </button>
+        </header>
 
         <div ref="example" className="drop-example">
           <div className="drop-scroll-content">
-            <TetherTarget
-              tethered={content(isOpen && toggleContent)}
-              tetherOptions={{
-                attachment: 'top center',
-                constraints: [
-                  {
-                    to: 'scrollParent',
-                    attachment: 'together'
-                  }
-                ]
+            <div
+              ref="target"
+              style={{
+                width: 200,
+                height: toggleHeight ? 300 : 200,
+                padding: 12,
+                background: '#b4da55',
               }}
-            >
-              <div
-                ref="target"
-                style={{
-                  width: 200,
-                  height: toggleHeight ? 300 : 200,
-                  padding: 12,
-                  background: '#b4da55',
+              onClick={() => this.setState({isOpen: !isOpen})}
+            />
+            {
+              isOpen &&
+              <TetherElement
+                target={this.refs.target}
+                options={{
+                  attachment: `${vertical} ${horizontal}`,
+                  constraints: [
+                    {
+                      to: 'scrollParent',
+                      attachment: 'together'
+                    }
+                  ]
                 }}
-                onClick={() => this.setState({isOpen: !isOpen})}
-              />
-            </TetherTarget>
+              >
+                <div
+                  style={{
+                    padding: 12,
+                    background: '#FF9800'
+                  }}
+                >
+                  Dropped Content
+                  {
+                    toggleContent &&
+                    <div>Can have state too :)</div>
+                  }
+                </div>
+              </TetherElement>
+            }
           </div>
         </div>
       </div>
