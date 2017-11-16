943258771c09010d9b84b45f9b88573e0c5cebeb
Dan Reeves <hey@danreev.es>
2017-11-16 12:53:55 +0000

    fix test

--- tests/test.js
+++ tests/test.js
@@ -7,12 +7,14 @@ const yOptions = ySelect.find('option');
 const xSelect = Selector('select').nth(1);
 const xOptions = xSelect.find('option');
 const heightButton = Selector('button').nth(2);
+const target = Selector('.drop-scroll-content');
 
 test('My first test', async t => {
   await t
+    .hover(target)
     .click(ySelect)
     .click(yOptions.withText('Top'))
-    .click(ySelect)
-    .click(yOptions.withText('Left'))
+    .click(xSelect)
+    .click(xOptions.withText('Left'))
     .click(heightButton);
 });
