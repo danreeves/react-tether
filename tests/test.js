a387c12f724bc4813a9e069e8ce00bcad3951ce4
Dan Reeves <hey@danreev.es>
2017-11-16 22:31:46 +0000

    readme tweak

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe';
 
-fixture`It doesn't crash`.page`http://localhost:8080/`;
+fixture`My first test`.page`http://localhost:8080/`;
 
 const ySelect = Selector('select').nth(0);
 const yOptions = ySelect.find('option');
@@ -9,7 +9,7 @@ const xOptions = xSelect.find('option');
 const heightButton = Selector('button').nth(2);
 const target = Selector('.drop-scroll-content');
 
-test('My first test', async t => {
+test(`It doesn't crash`, async t => {
   await t
     .hover(target)
     .click(ySelect)
