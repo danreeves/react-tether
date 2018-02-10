6ee961b3e0be5a0ee666573be06070fd9e96a021
Dan Reeves <hey@danreev.es>
2018-02-10 22:22:47 +0000

    Add Danger (#66)

---
+++ dangerfile.js
@@ -0,0 +1,27 @@
+import { warning, fail, message, danger } from 'danger';
+
+const modified = danger.git.modified_files;
+const modifiedSrc = modified.filter(p => p.includes('src/'));
+const changelogChanges = modified.find(f => f === 'CHANGELOG.md');
+const testChanges = modified.filter(p => p.includes('tests/'));
+
+message(
+  `Hey @${
+    danger.github.pr.user.login
+  }, thanks for submitting a pull request! :smile_cat:`
+);
+
+// Updates to the source require changelog updates
+if (modifiedSrc.length > 1 && !changelogChanges) {
+  warning(`You changed a source file but didn't add to the changelog`);
+}
+
+// Pull requests should have descriptions
+if (danger.github.pr.body.length === 0) {
+  warning('Please add a description to your PR');
+}
+
+// You added tests :tada:
+if (testChanges.length > 0) {
+  message(':tada: Thanks for working on tests!');
+}
