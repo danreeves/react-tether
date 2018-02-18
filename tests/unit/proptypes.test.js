5d293843fb887a13bcec42893bf55f6bd890d77d
Dan Reeves <hey@danreev.es>
2018-02-18 15:28:16 +0000

    Add unit tests (#68)
    * initial tests and move around
    
    * only build master branch
    
    * support testing multiple versions of react in jest/enzyme
    
    * add more tests, run multiple react versions in ci
    
    * add more tests
    
    * fix test with workaround
    
    * add more proptype tests
    
    * move danger to ci script

---
+++ tests/unit/proptypes.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { shallow, mount } from 'enzyme';
+import TetherComponent from '../../lib/react-tether';
+
+describe('propTypes', () => {
+  describe('children', () => {
+    const childrenProp = TetherComponent.propTypes.children;
+
+    it('should return an error if it has no children', () => {
+      const err = childrenProp(
+        { children: null },
+        'children',
+        'TetherComponent'
+      );
+      expect(err).toBeInstanceOf(Error);
+      expect(err.toString()).toContain('expects at least one child');
+    });
+
+    it('should return an error if it has more than 2 children', () => {
+      const err = childrenProp(
+        { children: [1, 2, 3] },
+        'children',
+        'TetherComponent'
+      );
+      expect(err).toBeInstanceOf(Error);
+      expect(err.toString()).toContain('Only a max of two children allowed');
+    });
+  });
+
+  describe('renderElementTo', () => {
+    const renderElementTo = TetherComponent.propTypes.renderElementTo;
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => () => {});
+
+    beforeEach(() => {
+      errorSpy.mockClear();
+    });
+
+    afterAll(() => {
+      errorSpy.mockRestore();
+    });
+
+    it('accepts strings', () => {
+      PropTypes.checkPropTypes(
+        { renderElementTo: TetherComponent.propTypes.renderElementTo },
+        { renderElementTo: 'a string' },
+        'renderElementTo',
+        'TetherComponent'
+      );
+      expect(errorSpy).not.toHaveBeenCalled();
+    });
+
+    it('accepts objects with the appendChild method', () => {
+      PropTypes.checkPropTypes(
+        { renderElementTo: TetherComponent.propTypes.renderElementTo },
+        { renderElementTo: { appendChild: () => {} } },
+        'renderElementTo',
+        'TetherComponent'
+      );
+      expect(errorSpy).not.toHaveBeenCalled();
+    });
+
+    it('only accepts appendChild as a function', () => {
+      PropTypes.checkPropTypes(
+        { renderElementTo: TetherComponent.propTypes.renderElementTo },
+        { renderElementTo: { appendChild: true } },
+        'renderElementTo',
+        'TetherComponent'
+      );
+      expect(errorSpy).toHaveBeenCalled();
+    });
+  });
+});
