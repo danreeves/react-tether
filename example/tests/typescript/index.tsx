c3925ff1b93110dcf8ab6082e5950ac5cd610f02
Dan Reeves <hey@danreev.es>
2018-04-22 00:22:14 +0100

    Dependency upgrades (#92)

--- example/tests/typescript/index.tsx
+++ example/tests/typescript/index.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReactTether from '../../../lib/react-tether';
 
-function App () {
-  return <div>
+function App() {
+  return (
+    <div>
       <h1>TypeScript example</h1>
       <ReactTether attachment="top left">
         <span>Child 1</span>
         <span>Child 2</span>
       </ReactTether>
     </div>
+  );
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
