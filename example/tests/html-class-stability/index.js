b41a45abc6b5e869a2f832f26adee1b4e123bea2
Dan Reeves <hey@danreev.es>
2019-10-25 16:44:40 +0100

    fix example for 1.x version

--- example/tests/html-class-stability/index.js
+++ example/tests/html-class-stability/index.js
@@ -26,47 +26,45 @@ class App extends React.Component {
               attachment: 'together',
             },
           ]}
-          renderTarget={ref => (
-            <div ref={ref}>
-              <button
-                onClick={() => {
-                  this.setState({ isOpen: !isOpen });
-                }}
-              >
-                Toggle Tethered Content
-              </button>
+        >
+          <div>
+            <button
+              onClick={() => {
+                this.setState({ isOpen: !isOpen });
+              }}
+            >
+              Toggle Tethered Content
+            </button>
+            <button
+              onClick={() => {
+                this.setState({
+                  classes: `flyout rand-class-${Math.floor(
+                    Math.random() * 100
+                  )}`,
+                });
+              }}
+            >
+              Change Classes
+            </button>
+          </div>
+
+          {isOpen && (
+            <div className="tether-content">
+              <h2>Tethered Content</h2>
+              <p>A paragraph to accompany the title.</p>
               <button
                 onClick={() => {
                   this.setState({
-                    classes: `flyout rand-class-${Math.floor(
-                      Math.random() * 100
-                    )}`,
+                    content: content + ' MORE CONTENT',
                   });
                 }}
               >
-                Change Classes
+                Add More Content
               </button>
+              <p>{content}</p>
             </div>
           )}
-          renderElement={ref =>
-            isOpen && (
-              <div className="tether-content" ref={ref}>
-                <h2>Tethered Content</h2>
-                <p>A paragraph to accompany the title.</p>
-                <button
-                  onClick={() => {
-                    this.setState({
-                      content: content + ' MORE CONTENT',
-                    });
-                  }}
-                >
-                  Add More Content
-                </button>
-                <p>{content}</p>
-              </div>
-            )
-          }
-        />
+        </ReactTether>
       </>
     );
   }
