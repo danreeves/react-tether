35c9a8afe132157bb53546360a5b64afef619eb6
Dan Reeves <hey@danreev.es>
2022-09-15 14:24:28 +0100

    microbundle

--- example/components/page-title.tsx
+++ example/components/page-title.tsx
@@ -5,71 +5,71 @@ import Target from "./target";
 import Tooltip from "./tooltip";
 
 const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem;
-  padding-bottom: 8rem;
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1rem;
+	padding-bottom: 8rem;
 `;
 
 const Title = styled.h1`
-  color: ${({ theme }) => theme.lightText};
-  font-family: ${({ theme }) => theme.font};
-  letter-spacing: -4px;
-  word-spacing: 0.5rem;
-  margin: 1rem;
-  font-size: 3rem;
-  display: inline-block;
+	color: ${({ theme }) => theme.lightText};
+	font-family: ${({ theme }) => theme.font};
+	letter-spacing: -4px;
+	word-spacing: 0.5rem;
+	margin: 1rem;
+	font-size: 3rem;
+	display: inline-block;
 `;
 
 const SIDES = ["middle left", "top center", "middle right", "top center"];
 
 function direction(tetherString: string): string {
-  return (tetherString.match(/left|top|right/) || [])[0] ?? "right";
+	return (tetherString.match(/left|top|right/) || [])[0] ?? "right";
 }
 
 type Props = {
-  children: React.ReactElement;
+	children: string;
 };
 export default function PageTitle(props: Props) {
-  let theme = useTheme();
-  let [index, setIndex] = useState(0);
+	let theme = useTheme();
+	let [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      setIndex((index) => (index + 1) % 4);
-    }, 3000);
+	useEffect(() => {
+		let interval = setInterval(() => {
+			setIndex((index) => (index + 1) % 4);
+		}, 3000);
 
-    return () => clearInterval(interval);
-  }, []);
+		return () => clearInterval(interval);
+	}, []);
 
-  const side = window.innerWidth > 750 ? SIDES[index] : "top center";
+	const side = window.innerWidth > 750 ? SIDES[index] : "top center";
 
-  return (
-    <Wrapper>
-      <TetherComponent
-        attachment={side}
-        constraints={[
-          {
-            to: "scrollParent",
-            attachment: "together",
-          },
-        ]}
-        renderTarget={(ref) => (
-          <Target
-            ref={ref}
-            height={100}
-            width={100}
-            color={theme.colors[index] || "red"}
-          />
-        )}
-        renderElement={(ref) => (
-          <Tooltip ref={ref} side={direction(side)}>
-            <Title>{props.children}</Title>
-          </Tooltip>
-        )}
-      />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<TetherComponent
+				attachment={side}
+				constraints={[
+					{
+						to: "scrollParent",
+						attachment: "together",
+					},
+				]}
+				renderTarget={(ref) => (
+					<Target
+						ref={ref}
+						height={100}
+						width={100}
+						color={theme.colors[index] || "red"}
+					/>
+				)}
+				renderElement={(ref) => (
+					<Tooltip ref={ref} side={direction(side)}>
+						<Title>{props.children}</Title>
+					</Tooltip>
+				)}
+			/>
+		</Wrapper>
+	);
 }
--- src/react-tether.tsx
+++ src/react-tether.tsx
@@ -8,6 +8,7 @@ if (!Tether) {
 	);
 }
 
+// @ts-expect-error
 const childrenPropType = ({ children }, propName, componentName) => {
 	const childCount = Children.count(children);
 	if (childCount > 0) {
@@ -17,6 +18,7 @@ const childrenPropType = ({ children }, propName, componentName) => {
 	}
 };
 
+// @ts-expect-error
 const attachmentPositions = [
 	"auto auto",
 	"top left",
@@ -46,6 +48,7 @@ class TetherComponent extends Component {
 
 	_tetherInstance = null;
 
+	// @ts-expect-error
 	componentDidMount() {
 		this._createContainer();
 		// The container is created after mounting
@@ -56,10 +59,13 @@ class TetherComponent extends Component {
 		this.forceUpdate();
 	}
 
+	// @ts-expect-error
 	componentDidUpdate(previousProps) {
 		// If the container related props have changed, then update the container
 		if (
+			// @ts-expect-error
 			previousProps.renderElementTag !== this.props.renderElementTag ||
+			// @ts-expect-error
 			previousProps.renderElementTo !== this.props.renderElementTo
 		) {
 			this._createContainer();
@@ -68,23 +74,30 @@ class TetherComponent extends Component {
 		// Verify if className props have changed
 		if (
 			this._elementParentNode &&
+			// @ts-expect-error
 			previousProps.className !== this.props.className
 		) {
 			// Add a bunch of checks against weird classNames
 			const previousClasses = (previousProps.className || "")
 				.split(" ")
+				// @ts-expect-error
 				.filter((value) => value.length > 0);
+			// @ts-expect-error
 			const currClasses = (this.props.className || "")
 				.split(" ")
+				// @ts-expect-error
 				.filter((value) => value.length > 0);
 
+			// @ts-expect-error
 			this._elementParentNode.classList.remove(...previousClasses);
+			// @ts-expect-error
 			this._elementParentNode.classList.add(...currClasses);
 		}
 
 		this._update();
 	}
 
+	// @ts-expect-error
 	componentWillUnmount() {
 		this._destroy();
 	}
@@ -94,26 +107,35 @@ class TetherComponent extends Component {
 	}
 
 	disable() {
+		// @ts-expect-error
 		this._tetherInstance.disable();
 	}
 
 	enable() {
+		// @ts-expect-error
 		this._tetherInstance.enable();
 	}
 
+	// @ts-expect-error
 	on(event, handler, ctx) {
+		// @ts-expect-error
 		this._tetherInstance.on(event, handler, ctx);
 	}
 
+	// @ts-expect-error
 	once(event, handler, ctx) {
+		// @ts-expect-error
 		this._tetherInstance.once(event, handler, ctx);
 	}
 
+	// @ts-expect-error
 	off(event, handler) {
+		// @ts-expect-error
 		this._tetherInstance.off(event, handler);
 	}
 
 	position() {
+		// @ts-expect-error
 		this._tetherInstance.position();
 	}
 
@@ -122,12 +144,16 @@ class TetherComponent extends Component {
 		// Later, when the component is mounted, the ref functions will be called
 		// and trigger a tether update
 		let targetComponent =
+			// @ts-expect-error
 			typeof this.props.renderTarget === "function"
-				? this.props.renderTarget(this._targetNode)
+				? // @ts-expect-error
+				this.props.renderTarget(this._targetNode)
 				: null;
 		let elementComponent =
+			// @ts-expect-error
 			typeof this.props.renderElement === "function"
-				? this.props.renderElement(this._elementNode)
+				? // @ts-expect-error
+				this.props.renderElement(this._elementNode)
 				: null;
 
 		// Check if what has been returned is a valid react element
@@ -145,17 +171,20 @@ class TetherComponent extends Component {
 		};
 	}
 
+	// @ts-expect-error
 	_createTetherInstance(tetherOptions) {
 		if (this._tetherInstance) {
 			this._destroy();
 		}
 
+		// @ts-expect-error
 		this._tetherInstance = new Tether(tetherOptions);
 		this._registerEventListeners();
 	}
 
 	_destroyTetherInstance() {
 		if (this._tetherInstance) {
+			// @ts-expect-error
 			this._tetherInstance.destroy();
 
 			this._tetherInstance = null;
@@ -163,18 +192,23 @@ class TetherComponent extends Component {
 	}
 
 	_registerEventListeners() {
+		// @ts-expect-error
 		this.on("update", (...args) => {
+			// @ts-expect-error
 			return this.props.onUpdate && this.props.onUpdate.apply(this, args);
 		});
 
+		// @ts-expect-error
 		this.on("repositioned", (...args) => {
 			return (
+				// @ts-expect-error
 				this.props.onRepositioned && this.props.onRepositioned.apply(this, args)
 			);
 		});
 	}
 
 	get _renderNode() {
+		// @ts-expect-error
 		const { renderElementTo } = this.props;
 		if (typeof renderElementTo === "string") {
 			return document.querySelector(renderElementTo);
@@ -192,22 +226,27 @@ class TetherComponent extends Component {
 		// Create element node container if it hasn't been yet
 		this._removeContainer();
 
+		// @ts-expect-error
 		const { renderElementTag, className } = this.props;
 
 		// Create a node that we can stick our content Component in
 		this._elementParentNode = document.createElement(renderElementTag);
+		// @ts-expect-error
 		this._elementParentNode.className = className || "";
 	}
 
 	_addContainerToDOM() {
 		// Append node to the render node
+		// @ts-expect-error
 		if (this._elementParentNode.parentNode !== this._renderNode) {
 			this._renderNode.append(this._elementParentNode);
 		}
 	}
 
 	_removeContainer() {
+		// @ts-expect-error
 		if (this._elementParentNode && this._elementParentNode.parentNode) {
+			// @ts-expect-error
 			this._elementParentNode.remove();
 		}
 	}
@@ -228,13 +267,21 @@ class TetherComponent extends Component {
 
 	_updateTether() {
 		const {
+			// @ts-expect-error
 			children,
+			// @ts-expect-error
 			renderElementTag,
+			// @ts-expect-error
 			renderElementTo,
+			// @ts-expect-error
 			id,
+			// @ts-expect-error
 			className, // This prop is specific to this._elementParentNode
+			// @ts-expect-error
 			style,
+			// @ts-expect-error
 			renderTarget,
+			// @ts-expect-error
 			renderElement,
 			...options
 		} = this.props;
@@ -245,11 +292,14 @@ class TetherComponent extends Component {
 		};
 
 		const idString = id || "";
+		// @ts-expect-error
 		if (this._elementParentNode.id !== idString) {
+			// @ts-expect-error
 			this._elementParentNode.id = idString;
 		}
 
 		if (style) {
+			// @ts-expect-error
 			const elementStyle = this._elementParentNode.style;
 			for (const key of Object.keys(style)) {
 				if (elementStyle[key] !== style[key]) {
@@ -261,14 +311,17 @@ class TetherComponent extends Component {
 		this._addContainerToDOM();
 
 		if (this._tetherInstance) {
+			// @ts-expect-error
 			this._tetherInstance.setOptions(tetherOptions);
 		} else {
 			this._createTetherInstance(tetherOptions);
 		}
 
+		// @ts-expect-error
 		this._tetherInstance.position();
 	}
 
+	// @ts-expect-error
 	render() {
 		const { targetComponent, elementComponent } = this._runRenders();
 
